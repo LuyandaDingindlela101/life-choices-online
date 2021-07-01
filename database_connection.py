73199931c3fbca5ae0eb40d95dd07c7dc441859e
LuyandaDingindlela101 <3995219@myuwc.ac.za>
2021-07-01 15:09:51 +0200

    added database connection - still not working though

--- database_connection.py
+++ database_connection.py
@@ -1,3 +1,22 @@
 import mysql.connector
 
-my_db = mysql.connector.connect(user='lifechoices', password='@Lifechoices1234', host='127.0.0.1', database='Hospital', auth_plugin='mysql_native_password')
+my_db = mysql.connector.connect(user='epiz_29025234', password='ldke7M8igIbjSrB', host='sql303.epizy.com', database='epiz_29025234_XXX', auth_plugin='mysql_native_password')
+
+
+def read_database():
+    my_cursor = my_db.cursor()
+    my_cursor.execute('SELECT * FROM visitors')
+
+    for i in my_cursor:
+        print(i)
+
+
+def describe_database():
+    my_cursor = my_db.cursor()
+    my_cursor.execute('DESCRIBE visitors')
+
+    for i in my_cursor:
+        print(i)
+
+
+read_database()
