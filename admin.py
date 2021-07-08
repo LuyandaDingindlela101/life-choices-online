031ce3ae1f925a17ed97ba260a88594fd37f438d
LuyandaDingindlela101 <3995219@myuwc.ac.za>
2021-07-08 13:57:14 +0200

    admin can now register users, now busy with validation on both the admin create and admin edit visitors functions

--- admin.py
+++ admin.py
@@ -1,11 +1,18 @@
 from tkinter import *
+from datetime import datetime
 from tkinter import messagebox
+from database_connection import *
 from tkinter.ttk import Style, Treeview
-from database_connection import read_table, delete_entry, select_from_table, update_table
 
 window = Tk()
 window.geometry("1100x550")
 window.title("Life Choices Online")
+
+#   ADD STYLES TO TREEVIEW
+style = Style()
+#   CHANGE THE BODY FONT
+style.configure("Treeview", highlightthickness=0, bd=0, bg="#fffffff", fieldbackground="#ffffff", fg="#8dc63f", font=('Helvetica', 11))
+style.map("Treeview", background=[("selected", "#8dc63f")])
 
 
 #   FUNCTION WILL POPULATE THE TREEVIEW WITH ALL THE USERS
@@ -98,50 +105,73 @@ def edit_visitor():
     window.geometry("1100x500")
 
 
+def show_hidden_entries():
+    #   MAKE SPACE FOR THE NEW WIDGETS BY INCREASING THE WINDOWS LENGTH
+    window.geometry("1100x1000")
+    #   PLACE THE WIDGETS SO THEY CAN BE SEEN
+    hr_label.place(x=10, y=500)
+    edit_label.place(x=400, y=550)
+    name_label.place(x=10, y=600)
+    name_entry.place(x=10, y=620)
+    surname_label.place(x=250, y=600)
+    surname_entry.place(x=250, y=620)
+    id_number_label.place(x=500, y=600)
+    id_number_entry.place(x=500, y=620)
+
+    phone_number_label.place(x=750, y=600)
+    phone_number_entry.place(x=750, y=620)
+    nok_label.place(x=400, y=700)
+    nok_name_label.place(x=10, y=750)
+    nok_name_entry.place(x=10, y=780)
+
+    nok_phone_number_label.place(x=250, y=750)
+    nok_phone_number_entry.place(x=250, y=780)
+
+
 def add_users():
-    pass
+    show_hidden_entries()
+    edit_visitor_btn.place(x=10, y=850)
 
 
 #   FUNCTION WILL EDIT A SELECTED ENTRY IN THE DATABASE
 def edit_row():
-    try:
-        #   MAKE SPACE FOR THE NEW WIDGETS BY INCREASING THE WINDOWS LENGTH
-        window.geometry("1100x1000")
-        #   PLACE THE WIDGETS SO THEY CAN BE SEEN
+    show_hidden_entries()
+    edit_visitor_btn.place(x=10, y=850)
 
-        hr_label.place(x=10, y=500)
-        edit_label.place(x=400, y=550)
-        name_label.place(x=10, y=600)
-        name_entry.place(x=10, y=620)
-        surname_label.place(x=250, y=600)
-        surname_entry.place(x=250, y=620)
-        id_number_label.place(x=500, y=600)
-        id_number_entry.place(x=500, y=620)
-
-        phone_number_label.place(x=750, y=600)
-        phone_number_entry.place(x=750, y=620)
-        nok_label.place(x=400, y=700)
-        nok_name_label.place(x=10, y=750)
-        nok_name_entry.place(x=10, y=780)
-
-        nok_phone_number_label.place(x=250, y=750)
-        nok_phone_number_entry.place(x=250, y=780)
-
-        edit_visitor_btn.place(x=10, y=850)
-
-        #   CALL THE populate_entries FUNCTION TO POPULATE THE ENTRIES WITH THE VISITOR AND NEXT OF KIN DETAILS
-        populate_entries()
-    except IndexError:
-        messagebox.showerror("Entry not chosen", "Please select an entry above")
+    #   CALL THE populate_entries FUNCTION TO POPULATE THE ENTRIES WITH THE VISITOR AND NEXT OF KIN DETAILS
+    populate_entries()
 
 
-style = Style()
-#   CHANGE THE BODY FONT
-style.configure("my_style.Treeview", highlightthickness=0, bd=0, font=('Helvetica', 11))
-#   CHANGE THE HEADING FONT
-style.configure("my_style.Treeview.Heading", font=('Helvetica', 13, 'bold'))
-#   REMOVE THE BORDERS
-style.layout("my_style.Treeview", [('my_style.Treeview.treearea', {'sticky': 'nswe'})])
+def create_row():
+    visitor_id = IntVar
+    show_hidden_entries()
+    create_visitor_btn.place(x=10, y=850)
+
+    #   GET THE visitor DETAILS FROM THE ENTRIES
+    name = name_entry.get()
+    surname = surname_entry.get()
+    id_number = id_number_entry.get()
+    phone_number = phone_number_entry.get()
+
+    #   GET THE nok DETAILS FROM THE ENTRIES
+    nok_name = nok_name_entry.get()
+    nok_phone_number = nok_phone_number_entry.get()
+    time_in = datetime.now()
+    #   CALL THE insert_visitor FUNCTION AND PASS IN THE NEEDED PARAMETERS
+    insert_visitor(name, surname, id_number, phone_number, 1, time_in)
+    # SELECT STATEMENT TO GET A DATABASE ENTRY THAT MEETS THE WHERE CLAUSE SO WE KNOW WHICH visitor TO ASSIGN THE NEXT OF KIN TO
+    query = "SELECT id FROM visitors WHERE name='" + name + "' AND id_number='" + id_number + "';"
+    #   CALL THE select_from_table AND PASS IN THE QUERY, WHICH RETURNS A LIST
+    db_rows = select_from_table(query)
+    #   HERE, WE LOOP THROUGH THE SET AT THE 0 INDEX TO GET THE VALUE OF THE id
+    for i in db_rows[0]:
+        visitor_id = i
+
+    #   SAVE THE NEXT OF KIN WITH THE CORRECT visitor_id
+    insert_nok(nok_name, nok_phone_number, visitor_id)
+    #   REPOPULATE THE TREEVIEW WITH THE NEW VISITOR
+    populate_treeview()
+
 
 canvas = Canvas(window, width=450, height=100)
 canvas.place(x=300, y=10)
@@ -179,7 +209,7 @@ tree_view.heading('Time Out', text="Time Out", anchor=CENTER)
 populate_treeview()
 tree_view.place(x=10, y=200)
 
-create_btn = Button(window, text="CREATE NEW VISITOR", bg="#8dc63f", fg="#ffffff", borderwidth=0, width=30)
+create_btn = Button(window, text="CREATE NEW VISITOR", bg="#8dc63f", fg="#ffffff", borderwidth=0, width=30, command=create_row)
 create_btn.place(x=10, y=450)
 
 edit_btn = Button(window, text="EDIT VISITOR", bg="#8dc63f", fg="#ffffff", borderwidth=0, width=30, command=edit_row)
@@ -216,5 +246,8 @@ nok_phone_number_entry = Entry(window)
 
 edit_visitor_btn = Button(window, text="Update visitor", bg="#8dc63f", fg="#ffffff", borderwidth=0, width=30,
                           command=edit_visitor)
+
+create_visitor_btn = Button(window, text="Add new visitor", bg="#8dc63f", fg="#ffffff", borderwidth=0, width=30,
+                          command=create_row)
 
 window.mainloop()
