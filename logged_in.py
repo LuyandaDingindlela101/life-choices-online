bd0bdfc9ba75e63dcb7765b5297fb39a3ce8580f
LuyandaDingindlela101 <3995219@myuwc.ac.za>
2021-07-09 00:26:07 +0200

    sign in and sign out are done, working on greeting the user on successfull sign in

--- logged_in.py
+++ logged_in.py
@@ -1,22 +1,28 @@
 from tkinter import *
 from tkinter import messagebox
-from database_connection import read_table
+from database_connection import select_from_table, update_table
 
 window = Tk()
 window.title("Life Choices Online")
 window.geometry("500x550")
 
 
-print(read_table("visitors"))
-
-
 def sign_out():
     sign_out = messagebox.askquestion("Sign out?", "Are you sure you want to sign out?")
 
     if sign_out == "yes":
+        #   FIRST, WE HAVE TO FIND OUT WHO IS LOGGED IN BY QUERYING THE DATABASE FOR WHERE logged_inn = 1
+        visitor = select_from_table("SELECT * FROM visitors WHERE logged_in = 1")[0]
+        #   CHANGE THEIR logged_in VALUE TO 0
+        update_table("UPDATE visitors SET logged_in = 0 WHERE id = " + str(visitor[0]))
         window.destroy()
         import sign_in
 
+
+def greet_user():
+    visitor = select_from_table("SELECT name FROM visitors WHERE logged_in = 1")[0]
+    print("id is: " + str(visitor[0]))
+    # messagebox.showinfo("Welcome", "Welcome back" + name + "!!")
 
 canvas = Canvas(window, width=450, height=100)
 canvas.place(x=10, y=10)
